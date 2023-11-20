bca4fcfdae010e030ed80f02813cbf3759c5ddab
Povilas Lekavičius <33901604+povilasleka@users.noreply.github.com>
2023-11-20 13:52:59 +0200

    fix driver email column length limit

--- db/migrate/20231115122609_update_driver_column_limits.rb
+++ db/migrate/20231115122609_update_driver_column_limits.rb
@@ -3,6 +3,6 @@ class UpdateDriverColumnLimits < ActiveRecord::Migration[7.1]
     change_column :drivers, :first_name, :string, limit: 50, null: false
     change_column :drivers, :last_name, :string, limit: 50, null: false
     change_column :drivers, :license_plate, :string, limit: 10, null: false
-    change_column :drivers, :email, :string, limit: 62, null: false
+    change_column :drivers, :email, :string, limit: 254, null: false
   end
 end
